c69611194edabed65fb811c085cd1af8aa1f36f6
JonathonMifsud <jonathon.mifsud1@students.mq.edu.au>
2023-09-26 17:24:20 +1000

    stop dir error for sed for setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -30,24 +30,31 @@ mkdir -p "${group_dir}"/{scripts,accession_lists,adapters,logs}
 mkdir -p "${scratch_dir}"/{abundance,read_count,raw_reads,trimmed_reads,ccmetagen,blast_results,annotation,mapping,contigs/{final_logs,final_contigs},fastqc,read_count}
 mkdir -p "${scratch_dir}"/abundance/final_abundance
 
-# Move all files from the current directory to the project's scripts directory
+# Move regular files (not directories) from the current directory to the project's scripts directory
 echo "Moving files to the project's scripts directory..."
-mv ./* "${group_dir}/scripts"
+find . -maxdepth 1 -type f -exec mv {} "${group_dir}/scripts" \;
 
 # Navigate to the project's scripts directory
 cd "${group_dir}/scripts"
 
 # Rename files with project name substitution
 echo "Renaming files with project name substitution..."
-find . -type f -exec bash -c 'new_file="${1/JCOM_pipeline/$2}"; [ "$1" != "$new_file" ] && mv "$1" "$new_file"' _ {} "$project" \;
+for file in *; do
+  new_file="${file/JCOM_pipeline/$project}"
+  [ "$file" != "$new_file" ] && mv "$file" "$new_file"
+done
 
-# Replace project-related variables in the script files
+# Replace project-related variables in the script files (excluding setup.sh)
 echo "Replacing project-related variables in the script files..."
-sed -i "s/JCOM_pipeline_virome/$project/g" *
-sed -i "s/JCOM_pipeline/$project/g" *
-sed -i "s/jcomvirome/$root/g" *
-sed -i "s/jmif9945@uni.sydney.edu.au/$email/g" *
-sed -i "s/jmif9945/$user/g" *
+for file in *; do
+  if [ "$file" != "setup.sh" ]; then
+    sed -i "s/JCOM_pipeline_virome/$project/g" "$file"
+    sed -i "s/JCOM_pipeline/$project/g" "$file"
+    sed -i "s/jcomvirome/$root/g" "$file"
+    sed -i "s/jmif9945@uni.sydney.edu.au/$email/g" "$file"
+    sed -i "s/jmif9945/$user/g" "$file"
+  fi
+done
 
 # Notify user about the project and scratch directory paths
 echo "Project setup completed successfully."
